cfd5e14cd392e499d84861980fd0f612dc0f5e46
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-26 14:22:01 -0300

    refatorando cruj v1.4

--- src/test/kotlin/br/com/zupacademy/giovannimoratto/BuscaAutorControllerTest.kt
+++ src/test/kotlin/br/com/zupacademy/giovannimoratto/BuscaAutorControllerTest.kt
@@ -2,15 +2,13 @@ package br.com.zupacademy.giovannimoratto
 
 import br.com.zupacademy.giovannimoratto.autor.*
 import br.com.zupacademy.giovannimoratto.core.endereco.Endereco
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -96,21 +94,49 @@ class BuscaAutorControllerTest {
 
     @Test
     internal fun `status 200 - lista de autores`() {
-
-
-        val response: HttpResponse<List<AutorResponse>> = client
+        val response = client
             .toBlocking()
-            .exchange("/api/autores")
+            .exchange("/api/autores", Any::class.java)
+//        val response = controller.buscar("")
+
+        val testResponse = listOf(
+            listOf(autor1.nome, autor1.email, autor1.descricao),
+            listOf(autor2.nome, autor2.email, autor2.descricao)
+        )
+
+
+//        val testResponse = listOf(
+//            {
+//                var nome = autor1.nome
+//                var email = autor1.email
+//                var descricao = autor1.descricao
+//            },
+//            {
+//                var nome = autor2.nome
+//                var email = autor2.email
+//                var descricao = autor2.descricao
+//            }
+//        )
 
         assertEquals(HttpStatus.OK, response.status)
         assertNotNull(response.body())
+        assertTrue(response.body()!!.toString().contains(autor1.nome))
+        assertTrue(response.body()!!.toString().contains(autor1.email))
+        assertTrue(response.body()!!.toString().contains(autor1.descricao))
+        assertTrue(response.body()!!.toString().contains(autor2.nome))
+        assertTrue(response.body()!!.toString().contains(autor2.email))
+        assertTrue(response.body()!!.toString().contains(autor2.descricao))
+        assertFalse(response.body()!!.toString().contains(autor1.cpf))
+        assertFalse(response.body()!!.toString().contains("endereco"))
+        assertFalse(response.body()!!.toString().contains("criadoEm"))
+        assertFalse(response.body()!!.toString().contains("id"))
     }
 
     @Test
     internal fun `status 200 - busca autor`() {
         val response = client
             .toBlocking()
-            .exchange("/api/autor?email=${autor1.email}", DetalhesDoAutorResponse::class.java)
+            .exchange("/api/autores?email=${autor1.email}", AutorResponse::class.java)
 
         assertEquals(HttpStatus.OK, response.status)
         assertNotNull(response.body())
@@ -119,16 +145,4 @@ class BuscaAutorControllerTest {
         assertEquals(autor1.email, response.body()!!.email)
     }
 
-    @Test
-    internal fun `status 200 - busca autor 2`() {
-        val response = client
-            .toBlocking()
-            .exchange("/api/autor2?email=${autor1.email}", AutorResponse::class.java)
-
-        assertEquals(HttpStatus.OK, response.status)
-        assertNotNull(response.body())
-        assertEquals(autor1.nome, response.body()!!.nome)
-        assertEquals(autor1.descricao, response.body()!!.descricao)
-        assertEquals(autor1.email, response.body()!!.email)
-    }
 }
